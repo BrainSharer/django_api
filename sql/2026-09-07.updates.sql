920eaa0db27784dd75036a4d642545afc3ce105a
Edward O'Donnell <eddy.odonnell@gmail.com>
2026-09-07 10:10:56 +0700

    Added new database table for notes

---
+++ sql/2026-09-07.updates.sql
@@ -0,0 +1,20 @@
+ALTER TABLE annotation_session ADD COLUMN FK_updated_by_id int(11) after FK_user_id;
+UPDATE annotation_session 
+SET FK_updated_by_id = FK_user_id 
+where TO_DAYS(created) != TO_DAYS(updated) and HOUR(created) = HOUR(updated);
+
+DROP TABLE IF EXISTS neuroglancer_log;
+CREATE TABLE `neuroglancer_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `FK_user_id` int(11) NOT NULL,
+  `FK_state_id` (int(11) NOT NULL,
+  `created` datetime(6) NOT NULL,
+  `change_message` longtext NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `K__FK_user_id` (`FK_user_id`),
+  KEY `K__FK_state_id` (`FK_state_id`),
+  CONSTRAINT `FK__auth_user_FK_user_id` FOREIGN KEY (`FK_user_id`) REFERENCES `auth_user` (`id`),
+  CONSTRAINT `FK__neuroglancer_state_FK_state_id` FOREIGN KEY (`FK_state_id`) REFERENCES `neuroglancer_state` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
